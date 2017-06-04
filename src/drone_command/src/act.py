2f246479af1e2b7656342f629ba313a23e098908
Ryan Owens <RyanOwens@linux.com>
2017-06-03 19:14:13 -0500

    Python setup

---
+++ src/drone_command/src/act.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python2
+
+# act...
+
+class Act(object):
+    @staticmethod
+    def act(plan):
+        pass
---
+++ src/drone_command/src/plan.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python2
+
+# plan...
+
+class Plan(object):
+    """docstring for Plan."""
+    def __init__(self, arg):
+        super(Plan, self).__init__()
+        self.arg = arg
